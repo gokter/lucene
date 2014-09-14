f0e1077e84ac2813e14a9c6add2bba17426a3139
Flycoding <Flycoding@yeah.net>
2014-09-14 21:01:35 +0800

    add searcher.
    Signed-off-by: Flycoding <Flycoding@yeah.net>

--- src/main/java/com/flyingh/lucene/HelloLucene.java
+++ src/main/java/com/flyingh/lucene/HelloLucene.java
@@ -9,8 +9,15 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Field.Index;
 import org.apache.lucene.document.Field.Store;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 
@@ -24,6 +31,17 @@ public class HelloLucene {
 				doc.add(new Field("fileName", file.getName(), Store.YES, Index.NOT_ANALYZED));
 				doc.add(new Field("path", file.getCanonicalPath(), Store.YES, Index.NOT_ANALYZED));
 				indexWriter.addDocument(doc);
+			}
+		}
+	}
+
+	public void searcher() throws CorruptIndexException, IOException, ParseException {
+		try (IndexSearcher indexSearcher = new IndexSearcher(IndexReader.open(FSDirectory.open(new File("C:/lucene/indices"))))) {
+			final TopDocs topDocs = indexSearcher.search(
+					new QueryParser(Version.LUCENE_35, "content", new StandardAnalyzer(Version.LUCENE_35)).parse("java"), 10);
+			for (final ScoreDoc scoreDoc : topDocs.scoreDocs) {
+				final Document doc = indexSearcher.doc(scoreDoc.doc);
+				System.out.println(doc.get("fileName") + "--->" + doc.get("path"));
 			}
 		}
 	}
--- src/test/java/com/flyingh/lucene/HelloLuceneTest.java
+++ src/test/java/com/flyingh/lucene/HelloLuceneTest.java
@@ -2,11 +2,28 @@ package com.flyingh.lucene;
 
 import java.io.IOException;
 
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.queryParser.ParseException;
+import org.junit.Before;
 import org.junit.Test;
 
 public class HelloLuceneTest {
+
+	private HelloLucene helloLucene;
+
+	@Before
+	public void before() {
+		helloLucene = new HelloLucene();
+	}
+
 	@Test
 	public void testIndex() throws IOException {
-		new HelloLucene().index();
+		helloLucene.index();
 	}
+
+	@Test
+	public void testSearcher() throws CorruptIndexException, IOException, ParseException {
+		helloLucene.searcher();
+	}
+
 }
